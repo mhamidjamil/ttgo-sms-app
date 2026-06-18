2595914582c44232f05ed7ad860bd3c19b01bb4d
mhamidjamil <55445517+mhamidjamil@users.noreply.github.com>
2026-06-18 16:40:00 +0500

    Raise the build to 1.6.2 for the field-test fixes #16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.spotwire.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.6.1"
+        versionCode = 11
+        versionName = "1.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
